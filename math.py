6f57f2ced8dca95c2270094ad453ae0f3f7422bf
shreyanagdive <shreya_nagdive@persistent.com>
2022-02-16 17:31:36 +0530

    Add fn headers

--- math.py
+++ math.py
@@ -1,16 +1,16 @@
-
+//add implementation
 def add(x,y):
     return x+y  
 
-
+//substract implementation
 def subtract(x,y):
     return x-y    
 
-
+//multiply implementation
 def multiply(x,y):
     return x*y
 
-
+//divide implementation
 def divide(x,y):
     return x/y
 
